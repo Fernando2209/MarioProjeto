acc62f0eac4556a38b77f08b9f28d912e08f2662
Laboratório Informática <labinfo@unifil.br>
2023-05-23 22:00:37 -0300

    Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('style/PaginaInicial/welcomeStyle.css')}}">
+    <title>Site de Aposta</title>
+</head>
+<body>
+    <div class="header">
+        <div class="header-info">
+            <div class="first-info-header">
+                <button class='btn-menu-header'>
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+            </div>
+            <div class="second-info-header">
+                <button class='btn-menu-header'>
+                    <i class="fa-solid fa-user"></i>
+                </button>
+                <button class='btn-menu-header'>
+                    <i class="fa-solid fa-gear"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="container-logo">
+            <h1>A Firma Logo!</h1>
+        </div>
+    </div>
+</body>
+</html>
